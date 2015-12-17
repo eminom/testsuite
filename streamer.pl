0b335858730d3c613b7338e0576ce426db4c9b6f
baihai <57270348@qq.com>
2015-12-17 10:12:44 +0800

    new streamer.pl

--- streamer.pl
+++ streamer.pl
@@ -12,20 +12,33 @@ sub uploadFile {
     my $full = shift // die;
     my $uri = "http://$host:$port/upload.jsp?name=$presub";
     my $cmd = "curl -i -T \"$full\" $uri";
-    print $cmd,"\n";
-    my $pid = fork;
-    die "Fork error" if !defined($pid);
-    if(0==$pid){
-        system($cmd);
-        exit;
+    #print $cmd,"\n";
+    #system($cmd);
+    `$cmd`;
+    die "upload for $uri failed" if $?;
+    #my $pid = fork;
+    #die "Fork error" if !defined($pid);
+    #if(0==$pid){
+    #    system($cmd);
+    #    exit;
+    #}
+}
+
+sub processorTask {
+    my $arr = shift // die "NO NO";
+    for my $ent(@{$arr}){
+        #my ($sub, $full) = @{$ent};
+        #print "$sub\n";
+        uploadFile @{$ent};
     }
 }
 
 # Parameters
 # 
-sub walkNow{
+sub walkNow {
     my $baseNow = shift // die "no base";   # $baseNow shall always ends with a slash. 
     my $dirNow  = shift // die "no dir for now";
+    my $refArr  = shift // die "no ref array for now";
     # my $pro_ref = shift // \&dummm;
     my @nextD;
     opendir my $cd, $dirNow or die "no open for \"$dirNow\"";
@@ -43,13 +56,46 @@ sub walkNow{
         #     # $pro_ref->($sub, $now);
         # }
         my $sub = substr($now, length($baseNow));
-        uploadFile $sub, $now;
+        push @{$refArr}, [$sub, $now];
+        #uploadFile $sub, $now;
     }
     closedir $cd;
-    walkNow($baseNow, $_) for @nextD;
+    walkNow($baseNow, $_, $refArr) for @nextD;
 }
 
-my $start = getcwd;
+if (!@ARGV) {
+    #print "Need target ip\n";
+    #exit -1;
+    $host = "localhost";
+} else {
+    $host = $ARGV[0];
+    $host = "localhost" if $host !~ /^(\d+)\.(\d+)\.(\d+)\.(\d+)$/i;
+}
+
+my $start = getcwd;    #Ends without slash
 die "not a source directory for $start" if not -d $start;
-walkNow($start.'/', $start);
-waitpid -1, 0;
+my @arr;
+walkNow $start.'/', $start, \@arr;
+
+#print scalar @arr, "\n";
+#print $#arr,"\n";
+#print "*"x20, "\n";
+
+my $count = scalar @arr;
+my $row = 4;
+my $col = int($count / $row);
+++$row if $count % $row;
+my @pids;
+while (@arr) {
+    my @jobs = splice @arr, 0, $col;
+    my $pid = fork;
+    if (0==$pid) {
+        processorTask(\@jobs);
+        exit;
+    } else {
+        push @pids, $pid;
+    }
+}
+#print "${$_}[0]\n${$_}[1]\n" for @arr;
+waitpid $_, 0 for @pids;
+print "done\n";
